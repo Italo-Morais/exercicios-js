69a24acdac7a4c9f8d21b66dae6b7453bb82bd81
Italo Morais <franciscoitalovianademorais@gmail.com>
2023-03-27 06:56:36 -0300

    fundamentals

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0); // infinity
+console.log('10' / 2); // coersion
+console.log('3' + 2); // concatenação
+console.log('3' - 2); // subtração
+console.log('Show!' * 2); // NaN
+console.log(0.1 + 0.7); // inprecisão
+// console.log(10.toString()); // SintaxError
+console.log((10.345).toFixed(2));
